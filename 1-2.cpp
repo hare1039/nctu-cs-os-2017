a6e83a0c4e9c01383576d5a0585611d0fb7b227c
hare1039 <hare39393@gmail.com>
2017-10-06 19:27:05 +0800

    Try to use IPC_PRIVATE as memory
    One step further toward complete

--- 1-2.cpp
+++ 1-2.cpp
@@ -14,15 +14,17 @@
 
 extern "C"
 {
-#include <unistd.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <sys/wait.h>
-#include <sys/time.h>
-#include <sys/mman.h>
-#include <sys/stat.h>        /* For mode constants */
-#include <fcntl.h> 
+    #include <unistd.h>
+    #include <sys/ipc.h>
+    #include <sys/shm.h>
+    #include <sys/wait.h>
+    #include <sys/time.h>
+    #include <sys/mman.h>
+    #include <sys/stat.h>        /* For mode constants */
+    #include <fcntl.h>
+    #include <errno.h>
 }
+
 
 class Process_manager
 {
@@ -35,7 +37,7 @@ public:
     {
         wait(NULL);
     }
-    std::list<std::unique_ptr<Process_manager> > child_processes;
+    std::list<std::unique_ptr<Process_manager>> child_processes;
     std::function<void(void)> context;
     
     Process_manager& register_child(std::function<void(void)> child_func)
@@ -50,7 +52,10 @@ public:
         return *this;
     }
 
-    
+	pid_t pid()
+    {
+		return getpid();
+    }
     void exec_all_child()
     {
         for(auto &i : child_processes)
@@ -67,10 +72,16 @@ public:
             }
         }
 		this->context();
+
 		long pending = child_processes.size();
 		while(pending)
 		{
-			wait(NULL);
+			pid_t end_pid = wait(NULL);
+			child_processes.remove_if([end_pid](std::unique_ptr<Process_manager> &mgr_ptr)
+				{
+					return mgr_ptr->pid() == end_pid;
+				}
+			);
 			pending--;
 		}
     }
@@ -94,10 +105,6 @@ int main(int argc, char *argv[])
 			for(int j(0); j < size; j++)	
 				for(int interator(0); interator < size; interator++)
 					matrix.at(pos_of(i, j)) += matrix_at(interator, j) * matrix_at(i, interator);
-//		for(auto i: matrix)
-//		{
-//			std::cout << "[no-fork] " << i << "\n";
-//		}
 		std::cout << "Time spent: " << std::chrono::duration_cast<std::chrono::milliseconds>(clock()).count()
 				  << " ms, check sum: " << static_cast<ulli>(std::accumulate(matrix.begin(), matrix.end(), 0)) << std::endl;
 
@@ -106,76 +113,82 @@ int main(int argc, char *argv[])
 	__start__ = std::chrono::steady_clock::now();
 	// fork & using System V IPC system
 	{
-		key_t key = ftok("Hi, I'm key", 'R'); // Please don't conflict
-		int shm_id = shmget( key, 4 * sizeof(ulli)/* 1 parent, 3 for children return partial sum*/, IPC_CREAT | 0660);
+		//key_t key = ftok("/tmp", 'm'); // Please don't conflict
+		//if(key < 0)
+		//	std::cerr << "[0] ftok failed, err: " << std::strerror(errno) << std::endl;
+		
+		int shm_id = shmget( IPC_PRIVATE, size * size * sizeof(ulli)/* matrix */, IPC_CREAT | 0666);
+		if(shm_id < 0)
+			std::cerr << "[0] shm_id dead, err: " << std::strerror(errno) << std::endl;
+
 		void * shm = shmat(shm_id, NULL, 0);
-		std::memset(shm, 0, 4 * sizeof(ulli));
-		Process_manager master([&key, &size, &pos_of]
-	    	{
-	 		    int shm_id = shmget( key, 4 * sizeof(ulli), S_IRUSR | 0660);
+		if(shm == (void *) -1)
+			std::cerr << "[0] shm failed, err: " << std::strerror(errno) << std::endl;
+
+		std::memset(shm, 0, size * size * sizeof(ulli));
+
+		
+		Process_manager master([&size, &pos_of, &shm_id]
+	    	{		
 	 		    void * shm = shmat(shm_id, NULL, 0);
-	 		    ulli * return_arr = static_cast<ulli *>(shm);
-				std::vector<ulli> matrix(size * size / 2);
+				if(shm == (void *) -1)
+					std::cerr << "[1] shm failed, err: " << std::strerror(errno) << std::endl;
+
+	 		    ulli * matrix = static_cast<ulli *>(shm);
+
+				int start_pos = 0;
 	 		    for(int i(0); i < size/4; i++)
-					for(int j(0); j < size; j++)	
+					for(int j(0); j < size; j++, start_pos++)
 						for(int interator(0); interator < size; interator++)
-							matrix.at(pos_of(i, j)) += pos_of(interator, j) * pos_of(i, interator);
-//				for(auto i: matrix)
-//				{
-//					std::cout << "[0~3] " << i << "\n";
-//				}
-				return_arr[0] = std::accumulate(matrix.begin(), matrix.end(), 0);
+							matrix[start_pos] += pos_of(interator, j) * pos_of(i, interator);
+				//std::cout << "[1] "<< start_pos << "\n";
 	    	}
 		);
-		master.register_child([&key, &size, &pos_of]
-		    {
-				int shm_id = shmget( key, 4 * sizeof(ulli), S_IRUSR | 0660);
+		master.register_child([&size, &pos_of, &shm_id]
+		    {		    	
 				void * shm = shmat(shm_id, NULL, 0);
-				ulli * return_arr = static_cast<ulli *>(shm);
-				std::vector<ulli> matrix(size * size / 2);
+				if(shm == (void *) -1)
+					std::cerr << "[2] shm failed, err: " << std::strerror(errno) << std::endl;
+
+				ulli * matrix = static_cast<ulli *>(shm);
+				int start_pos((size / 4) * size); 
 				for(int i(size/4); i < size/2; i++)
-					for(int j(0); j < size; j++)	
+					for(int j(0); j < size; j++, start_pos++)	
 						for(int interator(0); interator < size; interator++)
-							matrix.at(pos_of(i - size/4, j)) += pos_of(interator, j) * pos_of(i, interator);
-// 				for(auto i: matrix)
-// 				{
-// 					std::cout << "[4~7] " << i << "\n";
-// 				}
-				return_arr[1] = std::accumulate(matrix.begin(), matrix.end(), 0);
+							matrix[start_pos] += pos_of(interator, j) * pos_of(i, interator);
+				//std::cout << "[2] "<< start_pos << "\n";
 		    }
 		);
-		master.register_child([&key, &size, &pos_of]
-		    {
-				int shm_id = shmget( key, 4 * sizeof(ulli), S_IRUSR | 0660);
+		master.register_child([&size, &pos_of, &shm_id]
+		    {	
 				void * shm = shmat(shm_id, NULL, 0);
-				ulli * return_arr = static_cast<ulli *>(shm);
-				std::vector<ulli> matrix(size * size / 2);
+				if(shm == (void *) -1)
+					std::cerr << "[3] shm failed, err: " << std::strerror(errno) << std::endl;
+ 
+
+				ulli * matrix = static_cast<ulli *>(shm);
+				int start_pos((size / 2) * size);
 				for(int i(size/2); i < size*3/4; i++)
-					for(int j(0); j < size; j++)	
+					for(int j(0); j < size; j++, start_pos++)	
 						for(int interator(0); interator < size; interator++)
-							matrix.at(pos_of(i - size/2, j)) += pos_of(interator, j) * pos_of(i, interator);
-// 				for(auto i: matrix)
-// 				{
-// 					std::cout << "[8~11] " << i << "\n";
-// 				}
-				return_arr[2] = std::accumulate(matrix.begin(), matrix.end(), 0);
+							matrix[start_pos] += pos_of(interator, j) * pos_of(i, interator);
+				//std::cout << "[3] "<< start_pos << "\n";
 		    }
 		);
-		master.register_child([&key, &size, &pos_of]
+		master.register_child([&size, &pos_of, &shm_id]
 		    {
-				int shm_id = shmget( key, 4 * sizeof(ulli), S_IRUSR | 0660);
 				void * shm = shmat(shm_id, NULL, 0);
-				ulli * return_arr = static_cast<ulli *>(shm);
-				std::vector<ulli> matrix(size * size / 2);
+				if(shm == (void *) -1)
+					std::cerr << "[4] shm failed, err: " << std::strerror(errno) << std::endl;
+				
+ 
+				ulli * matrix = static_cast<ulli *>(shm);
+				int start_pos((size * 3 / 4) * size);
 				for(int i(size*3/4); i < size; i++)
-					for(int j(0); j < size; j++)	
+					for(int j(0); j < size; j++, start_pos++)	
 						for(int interator(0); interator < size; interator++)
-							matrix.at(pos_of(i - size*3/4, j)) += pos_of(interator, j) * pos_of(i, interator);
-//				for(auto i: matrix)
-//				{
-//					std::cout << "[12~15] " << i << "\n";
-//				}
-				return_arr[3] = std::accumulate(matrix.begin(), matrix.end(), 0);
+							matrix[start_pos] += pos_of(interator, j) * pos_of(i, interator);
+				//std::cout << "[4] "<< start_pos << "\n";
 		    }
 		);
 
@@ -183,10 +196,8 @@ int main(int argc, char *argv[])
 
 
 		ulli * result = static_cast<ulli *>(shm), sum(0);
-		for(int i(0); i<4; i++)		
-			sum += result[i];
-		
-			
+  		for(int i(0); i < size * size; i++)		
+  			sum += result[i];
 		shmdt(shm);
 	
 		std::cout << "Time spent: " << std::chrono::duration_cast<std::chrono::milliseconds>(clock()).count()
@@ -195,9 +206,3 @@ int main(int argc, char *argv[])
 
     return 0;
 }
-
-
-	//int shm_fd = shm_open("the_matrix", O_CREAT | O_RDWR, 0666);
-	//std::vector<int> matrix_shared(size * size);
-	//ftruncate(shm_fd, matrix_shared.size());
-	//void *ptr = mmap(0, matrix_shared.size(), PROT_WRITE, MAP_SHARED, shm_fd, 0);
